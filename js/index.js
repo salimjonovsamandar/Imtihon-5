f4fc04c545cd0cd1d0bae1b9d67dcfb407e3785e
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2024-01-20 15:48:33 +0500

    Imtixon bajarilyabdi

--- js/index.js
+++ js/index.js
@@ -3,11 +3,20 @@ const theme = document.getElementById('theme');
 const input = document.querySelector('.input');;
 const btn = document.getElementById('btn');
 const region = document.getElementById('region');
-const body = document.querySelector('body')
+const bodymain = document.querySelector('body')
+const body = document.querySelector('.Allbody')
+const loader = document.querySelector('.loader')
+
+body.style.display = "none";
+
+function loaderCallback() {
+    loader.style.display = "none";
+    body.style.display = "block";
+}
 
 function createCard(flag) {
     return `
-    <div class="card" id="${flag.name.slug}">
+    <div class="card card1" id="${flag.name.slug}">
                 <img src="${flag.flags.png}" alt="flag img">
                 <div class="title">
                     <h2>${ flag.name.common}</h2>
@@ -28,15 +37,16 @@ document.addEventListener('DOMContentLoaded', function () {
             data.data.forEach((flag) => {
                 let card = createCard(flag);
                 wrapper.innerHTML += card;
+
             });
+            loaderCallback()
         })
         .catch((err) => {
             console.log(err);
         });
 })
 
-
-btn && btn.addEventListener('click', function () {
+input.addEventListener('keyup', function () {
     let value = input.value;
     fetch(`https://countries-api-v7sn.onrender.com/countries?search=${value}`)
         .then(data => data.json())
@@ -51,7 +61,6 @@ btn && btn.addEventListener('click', function () {
             console.log(err);
         })
 })
-
 wrapper && wrapper.addEventListener('click', () => {
     const card = document.querySelectorAll(".card");
     card.forEach(cardAdd => {
@@ -61,3 +70,39 @@ wrapper && wrapper.addEventListener('click', () => {
         });
     });
 })
+
+region.addEventListener('change', function () {
+    let value = region.value;
+    if (value == "All") {
+        fetch("https://countries-api-v7sn.onrender.com/countries?limit=250", {
+                method: "GET",
+            })
+            .then(res => res.json())
+            .then(data => {
+                data.data.forEach((flag) => {
+                    let card = createCard(flag);
+                    wrapper.innerHTML += card;
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    fetch(`https://countries-api-v7sn.onrender.com/countries?region=${value}`)
+        .then(data => data.json())
+        .then(data => {
+            wrapper.innerHTML = ''
+            data.data.forEach(element => {
+                let card = createCard(element)
+                wrapper.innerHTML += card
+            });
+            loaderCallback()
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+theme && theme.addEventListener("click", () => {
+    bodymain.classList.toggle("darc")
+});
--- js/main.js
+++ js/main.js
@@ -1,44 +1,56 @@
+// console.log(window.location.href);
+const wrapper = document.getElementById('wrapper');
+let url = window.location.href.substring(38)
+const body = document.querySelector('.Allbody')
+const loader = document.querySelector('.loader')
+body.style.display = "none";
+
+function loaderCallback() {
+    loader.style.display = "none";
+    body.style.display = "block";
+}
 
 
+function createCard(flag) {
+    return `
+    <div class="main">
+                <div class="img">
+                    <img src="${flag.flags.png}" width="560" height="420"  alt="">
+                </div>
+                <div class="description_main">
+                    <div class="description">
+                    <div class="title">
+                        <h5>${flag.name.common}</h5>
+                        <p>Native Name: <span>${flag.languages}</span></p>
+                        <p>Population: <span>${flag.population}</span></p>
+                        <p>Region: <span>${flag.region}</span></p>
+                        <p>Sub Region: <span>${flag.subregion}</span></p>
+                        <p>Capital: <span>${flag.capital}</span></p>
+                    </div>
+                    <div class="text">
+                        <p>Top Level Domain: <span>be</span></p>
+                        <p>Currencies: <span>${flag.currencies}</span></p>
+                        <p>Languages: <span>${flag.languages}</span></p>
+                    </div>
+                </div>
+                <div class="footer">
+                    <h4>Border Countries: </h4>
 
+                </div>
+                </div>
+            </div>
+    `
+}
+document.addEventListener('DOMContentLoaded', function () {
+    fetch(`https://countries-api-v7sn.onrender.com/countries/slug/${url}`)
+   
+        .then(data => data.json())
+        .then(data => {
+            let card = createCard(data)
+            wrapper.innerHTML = card;
+            loaderCallback()
+            
+        })
+        
+})
 
-// function createCard(flag) {
-//     return `
-//     <div class="main">
-//                 <div class="img">
-//                     <img src="${flag.flags.png}" alt="">
-//                 </div>
-//                 <div class="description_main">
-//                     <div class="description">
-//                     <div class="title">
-//                         <h5>${flag.name.common}</h5>
-//                         <p>Native Name: <span>${flag.languages}</span></p>
-//                         <p>Population: <span>${flag.population}</span></p>
-//                         <p>Region: <span>${flag.region}</span></p>
-//                         <p>Sub Region: <span>${flag.subregion}</span></p>
-//                         <p>Capital: <span>${flag.capital}</span></p>
-//                     </div>
-//                     <div class="text">
-//                         <p>Top Level Domain: <span>be</span></p>
-//                         <p>Currencies: <span>${flag.currencies}</span></p>
-//                         <p>Languages: <span>${flag.languages}</span></p>
-//                     </div>
-//                 </div>
-//                 <div class="footer">
-//                     <h4>Border Countries: </h4>
-
-//                 </div>
-//                 </div>
-//             </div>
-//     `
-// }
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     fetch(`https://countries-api-v7sn.onrender.com/countries/slug/${countryName}`)
-//         .then(data => data.json())
-//         .then(data => {
-//             console.log(data);
-//         })
-// })
